8f65f9fc2089e5271e578e7123256af346a3aa30
azazell <azaliya_tagirova72@mail.ru>
2022-12-02 10:17:34 +0600

    Домашнее задание 4 сделано

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 public class Main {
-    public static void main(String[] args) {System.out.println("Task 1");
+    public static void main(String[] args) {
+        System.out.println("Task 1");
         Car lada = new Car("Лада", "Гранта", 59.5599, "Бурый", "Хетчбэк", "автомат");
         System.out.println(lada);
-        Car audi = new Car("Audi", "A8 50 L TDI quattro", 43.70,"Белый", null, "механика");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 43.70, "Белый", null, "механика");
         System.out.println(audi);
         Car bmw = new Car("BMW", "Z8", 0, "Чёрный", "Хетчбэк", "автомат");
         System.out.println(bmw);
-        Car kia = new Car("Kia", "Sportage 4-го поколения", 123.1234, null,  null, null);
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 123.1234, null, null, null);
         System.out.println(kia);
         kia.startMoving();
         System.out.println();
-        Bus paz3237 = new Bus("ПАЗ", "3237", 135.9,  null);
+        Bus paz3237 = new Bus("ПАЗ", "3237", 135.9, null);
         System.out.println(paz3237);
-        Bus pazV = new Bus("ПАЗ", "Вектор", 90,  "Серый");
+        Bus pazV = new Bus("ПАЗ", "Вектор", 90, "Серый");
         System.out.println(pazV);
         Bus neman = new Bus("Неман", "420211-10", 57.49, "Зеленый");
         System.out.println(neman);
@@ -30,13 +31,21 @@ public class Main {
         Truck gazel4 = new Truck("Газель", "Рута", 110);
         System.out.println(gazel4);
         gazel4.finishMoving();
-
-        neman.pitStop();
-        neman.maxSpeed();
-        neman.bestTime();
-        audi.pitStop();
-        gazel.maxSpeed();
-        kia.bestTime();
-
+//
+//        neman.pitStop();
+//        neman.maxSpeed();
+//        neman.bestTime();
+//        audi.pitStop();
+//        gazel.maxSpeed();
+//        kia.bestTime();
+        System.out.println();
+        System.out.println("Task 2.5.4");
+        CategoryBDriver azazel = new CategoryBDriver("ТАА", true, 12, audi);
+        System.out.println(azazel);
+        azazel.stop();
+        CategoryCDriver max = new CategoryCDriver("АМТ", true, 10, gazel4);
+        System.out.println(max);
+        CategoryDDriver dinislam = new CategoryDDriver("ГДА", true, 20, anonim);
+        System.out.println(dinislam);
     }
 }
